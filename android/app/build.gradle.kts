18135cacf27094b768281ece0c08769ca41a7c2a
Abduraxmon <abdugafurovabduraxmon@gmail.com>
2025-07-09 13:21:13 +0500

    create signinConfig

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,52 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("org.jetbrains.kotlin.android")
+   
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+// 🔐 Keystore ma'lumotlarini yuklab olish
+val keystoreProperties = Properties().apply {
+    load(FileInputStream(rootProject.file("key.properties")))
 }
 
 android {
     namespace = "com.example.numbersapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
+
+    defaultConfig {
+        applicationId = "com.abduraxmon.numbersapp"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            signingConfig = signingConfigs.getByName("release")
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -18,29 +56,8 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.numbersapp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-            signingConfig = signingConfigs.getByName("release")
-        }
-    }
 }
+
 
 flutter {
     source = "../.."
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -14,6 +14,9 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+     plugins {
+     
+    }
 }
 
 plugins {
